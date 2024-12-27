27a733915d2e7bde52eb4ccf933da8c53ae99e2b
xerasoo <erika.suseke@gmail.com>
2024-12-27 14:34:26 +0200

    admin configs added

--- backEnd/routes/adminRouter.js
+++ backEnd/routes/adminRouter.js
@@ -1,6 +1,10 @@
 import express from "express";
-// import * as adminController from "../controllers/adminController.js";
+import * as adminController from "../controllers/adminController.js";
 
 const router = express.Router();
 
+router.post("/login", adminController.login);
+router.post("/register", adminController.register);
+router.get("/logout", adminController.logout);
+router.get("/check-session", adminController.session);
 export default router;
