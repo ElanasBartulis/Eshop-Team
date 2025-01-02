482dca49d0489ad525e9e6b084f67849cf4539e1
ZilvinasStanius <zilvinas950203@gmail.com>
2025-01-02 14:40:50 +0200

    commit

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router";
-import Dashboard from "./pages/Dashboard";
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
+import Dashboard from './pages/Dashboard';
+import UserPanel from './pages/UsuerPanel';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route
+          path="/"
+          element={<Dashboard />}
+        />
+        <Route
+          path="/user"
+          element={<UserPanel />}
+        />
       </Routes>
     </BrowserRouter>
   );
