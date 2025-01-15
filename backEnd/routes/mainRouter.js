ade9eb6942feca9b7c11bd1fd6fc04d3537f39fb
ZilvinasStanius <zilvinas950203@gmail.com>
2025-01-15 18:20:26 +0200

    Padaryta rating sistema backende. Sutvarkytas blank screen page reload metu.

--- backEnd/routes/mainRouter.js
+++ backEnd/routes/mainRouter.js
@@ -3,10 +3,11 @@ import express from 'express';
 import productRouter from './ProductRouter.js';
 import userHistoryRouter from './userHistoryRouter.js';
 import userRouter from './userRouter.js';
+import ratingRouter from './ratingRouter.js';
 const router = express.Router();
 
 // http://localhost/server/api/
-
+router.use('/rating', ratingRouter);
 router.use('/users', userRouter);
 router.use('/product', productRouter);
 router.use('/history', userHistoryRouter);
---
+++ frontEnd/src/custom-hooks/useSessionCheck.js
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+
+const useSessionCheck = ({
+  setSessionState,
+  setUserData,
+  setIsCheckingSession,
+}) => {
+  useEffect(() => {
+    async function checkSession() {
+      try {
+        const promise = await fetch(
+          'http://localhost/server/api/users/session'
+        );
+        const response = await promise.json();
+        if (promise.ok && response.isLogged) {
+          setSessionState({ isLogged: true });
+          setUserData(response.user);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsCheckingSession(false);
+      }
+    }
+    checkSession();
+  }, [setSessionState, setUserData, setIsCheckingSession]);
+};
+
+export default useSessionCheck;
