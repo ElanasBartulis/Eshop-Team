234b4d743d319038d657cd799a3f45c4c13325b9
xerasoo <erika.suseke@gmail.com>
2025-02-05 13:18:23 +0200

    Fixed navigation

--- frontEnd/src/pages/Dashboard.jsx
+++ frontEnd/src/pages/Dashboard.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/DashboardHeader";
 import Main from "../components/DashboardMain";
 import Footer from "../components/Footer";
+import Nav from "../components/Navigation";
 
 export default function Dashboard() {
   return (
     <div className="container mx-auto px-4">
+      <Nav />
       <Header />
       <Main />
       <Footer />
